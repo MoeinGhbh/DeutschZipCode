a9e6821cc29a1a2abcd0a8afe3d24f870097de55
= <=>
2020-05-12 10:54:19 +0200

    format zipcode file

--- ZipCode.js
+++ ZipCode.js
@@ -1,18 +1,23 @@
-import {html, render} from 'lit-html';
+import {
+  html,
+  render
+} from 'lit-html';
 
 
 class ZipCode extends HTMLElement {
 
-    constructor(){
-        super();
-        this.shadow = this.attachShadow({"mode":"open"})
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({
+      "mode": "open"
+    })
 
-        const zipcodetemplate = html` <input type="text" pattern="[0-9]*" name="zip" id="zip"  >`
-        const citytemplate =  html`   <input type="text" name="city" id="city">`
-        const streettemplate = html`  <select id="street" name="street" ></select>`
+    const zipcodetemplate = html ` <input type="text" pattern="[0-9]*" name="zip" id="zip"  >`
+    const citytemplate = html `   <input type="text" name="city" id="city">`
+    const streettemplate = html `  <select id="street" name="street" ></select>`
 
 
-        const template = html`
+    const template = html `
 
 <style>
 html, body {
@@ -119,114 +124,114 @@ textarea {
 </div>
 `
 
-        render(template,this.shadowRoot)
+    render(template, this.shadowRoot)
 
-    }
-    
-
-    
-    findZipCode() {
-      var cityName=''
-      const zip = this.shadowRoot.querySelector('#zip');
-      const city = this.shadowRoot.querySelector('#city');
-      var alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l',
-      'm','n','o','p','q','r','s','t','u','v','w','x','y','z','ä','ö','ü'];
-      var x;
-      for (x of alphabets) {
-                                              fetch("https://www.postdirekt.de/plzserver/PlzAjaxServlet?autocomplete=plz&plz_city="+x,
-                                              {
-                                                method:'get'
-                                                    })
-                                                    .then(response => response.json())
-                                                    .then((response) => { 
-                                                return response.json().then((data) => {
-                                                    const mydata = data
-                                                    // console.log(data)
-                                                    var i = 0;
-                                                    var len = data["rows"].length;
-                                                    for (; i < len; ) {
-                                                      if ((data["rows"][i]["plz"]).split(/([0-9]+)/)[1]==String(zip.value).slice(0,(data["rows"][i]["plz"]).split(/([0-9]+)/)[1].length))
-                                                        {
-                                                          city.value=data["rows"][i]["city"]
-                                                          cityName=data["rows"][i]["city"]
-                                                          break;
-                                                        }
-                                                      i++;
-                                                    }
-                                                    return data;
-                                                })})
-                                                .catch(function(error){
-                                                            console.log(error)
-                                                    })
-                                      }
+  }
 
 
-                                      var alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','ä','ö','ü'];
-                                      var x;
-                                      
-                                      for (var j=0;j<alphabets.length;j++)
-                                      {
-                                        console.log(cityName,'ssssssssssssssssss')
-                                                console.log("https://www.postdirekt.de/plzserver/PlzAjaxServlet?autocomplete=street&plz_city="+city.value+"&plz_plz="+zip.value+"&plz_district=&plz_street="+alphabets[j])
-                                                                      fetch("https://www.postdirekt.de/plzserver/PlzAjaxServlet?autocomplete=street&plz_city="+city.value+"&plz_plz="+zip.value+"&plz_district=&plz_street="+alphabets[j],
-                                                                              {
-                                                                                method:'get'
-                                                                                    }).then((response) => { 
-                                                                                return response.json().then((data) => {
-                                                                                    const mydata = data
-                                                                                    console.log(data,'second')
-                                                                                    console.log(zip.value)
-                                                                                    
-                                                                                    
 
-                                                                                    
-                                                                                    var addStreet = this.shadowRoot.querySelector('#street');
-
-                    
-                                                  for (var r=0;r<data["rows"].length;r++)
-                                                                  {
-                                                  addStreet.options[addStreet.options.length] = new Option(data["rows"][r]["street"], data["rows"][r]["street"]);
-                                                                  }
-                                                                                  })})
-                                      }
+  findZipCode() {
+    var cityName = ''
+    const zip = this.shadowRoot.querySelector('#zip');
+    const city = this.shadowRoot.querySelector('#city');
+    var alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+      'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü'
+    ];
+    var x;
+    for (x of alphabets) {
+      fetch("https://www.postdirekt.de/plzserver/PlzAjaxServlet?autocomplete=plz&plz_city=" + x, {
+          method: 'get'
+        })
+        .then(response => response.json())
+        .then((response) => {
+          return response.json().then((data) => {
+            const mydata = data
+            // console.log(data)
+            var i = 0;
+            var len = data["rows"].length;
+            for (; i < len;) {
+              if ((data["rows"][i]["plz"]).split(/([0-9]+)/)[1] == String(zip.value).slice(0, (data["rows"][i]["plz"]).split(/([0-9]+)/)[1].length)) {
+                city.value = data["rows"][i]["city"]
+                cityName = data["rows"][i]["city"]
+                break;
+              }
+              i++;
+            }
+            return data;
+          })
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
     }
 
-    ShowInfo(){
-      const zip = this.shadowRoot.querySelector('#zip').value;
-      const street = this.shadowRoot.querySelector('#street').value;
-      const country = this.shadowRoot.querySelector('#country').value;
-      const houseNumber = this.shadowRoot.querySelector('#houseNumber').value;
-      const city = this.shadowRoot.querySelector('#city').value;
 
-      
-      console.log(street,city,country,zip,houseNumber);
+    var alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü'];
+    var x;
 
-      console.log(`   The Street is  ${street} 
+    for (var j = 0; j < alphabets.length; j++) {
+      console.log(cityName, 'ssssssssssssssssss')
+      console.log("https://www.postdirekt.de/plzserver/PlzAjaxServlet?autocomplete=street&plz_city=" + city.value + "&plz_plz=" + zip.value + "&plz_district=&plz_street=" + alphabets[j])
+      fetch("https://www.postdirekt.de/plzserver/PlzAjaxServlet?autocomplete=street&plz_city=" + city.value + "&plz_plz=" + zip.value + "&plz_district=&plz_street=" + alphabets[j], {
+        method: 'get'
+      }).then((response) => {
+        return response.json().then((data) => {
+          const mydata = data
+          console.log(data, 'second')
+          console.log(zip.value)
+
+
+
+
+          var addStreet = this.shadowRoot.querySelector('#street');
+
+
+          for (var r = 0; r < data["rows"].length; r++) {
+            addStreet.options[addStreet.options.length] = new Option(data["rows"][r]["street"], data["rows"][r]["street"]);
+          }
+        })
+      })
+    }
+  }
+
+
+
+  ShowInfo() {
+    const zip = this.shadowRoot.querySelector('#zip').value;
+    const street = this.shadowRoot.querySelector('#street').value;
+    const country = this.shadowRoot.querySelector('#country').value;
+    const houseNumber = this.shadowRoot.querySelector('#houseNumber').value;
+    const city = this.shadowRoot.querySelector('#city').value;
+
+
+    console.log(street, city, country, zip, houseNumber);
+
+    console.log(`   The Street is  ${street} 
                       and city is ${city}
                       and House Number ${houseNumber}
                       and country is ${country}
                       and zip is ${zip}.`);
-      alert(`         The Street is  ${street} 
+    alert(`         The Street is  ${street} 
                       and city is ${city}
                       and House Number ${houseNumber}
                       and country is ${country}
                       and zip is ${zip}.`)
-    }
+  }
 
 
-    connectedCallback() {
-      this.shadowRoot.querySelector('#zip')
+  connectedCallback() {
+    this.shadowRoot.querySelector('#zip')
       .addEventListener('blur', () => this.findZipCode());
 
-      this.shadowRoot.querySelector('#toggle-info')
+    this.shadowRoot.querySelector('#toggle-info')
       .addEventListener('click', () => this.ShowInfo());
 
-    }
+  }
 
-    disconnectedCallback() {
-      this.shadowRoot.querySelector('#zip').removeEventListener();
-    }
+  disconnectedCallback() {
+    this.shadowRoot.querySelector('#zip').removeEventListener();
+  }
 
 }
 
-window.customElements.define("zip-code",ZipCode)
+window.customElements.define("zip-code", ZipCode)
